8934ace020dbc64827872d7b23e763c92f403451
Dmitry <dimas1114@yandex.ru>
2019-07-20 14:26:07 +0300

    init

---
+++ src/main/kotlin/venus/searchcore/SearchCoreApplication.kt
@@ -0,0 +1,11 @@
+ackage venus.searchcore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<SearchCoreApplication>(*args)
+}
---
+++ src/test/kotlin/venus/searchcore/entity/TestEntity.kt
@@ -0,0 +1,15 @@
+package venus.searchcore.entity
+
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+class TestEntity (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long? = null,
+        val name: String? = null,
+        val date: LocalDate? = null,
+        val float: Float? = null,
+        @ManyToOne(cascade = [CascadeType.ALL])
+        var child: TestEntity2? = null)
